8796ec61d61e0cad62fb5c8fbf1297994bec3a90
sleeptodeath <1066049243@qq.com>
2019-09-19 22:32:42 +0800

    add 069

--- 069_Sqrt/src/Main.java
+++ 069_Sqrt/src/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
 
 public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.mySqrt1(4));
+        System.out.println(solution.mySqrt1(8));
+    }
 }
--- 069_Sqrt/src/Solution.java
+++ 069_Sqrt/src/Solution.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
 
 public class Solution {
+    // 二分搜索
+    public int mySqrt(int x) {
+        if (x == 0)
+            return 0;
+        int l = 1, r = x;
+        int mid;
+        while (l <= r) {
+            mid = l + (r-l) / 2;
+           if (mid > x / mid) {
+               r = mid - 1;
+           } else {
+               l = mid + 1;
+           }
+        }
+        return r;
+    }
+
+    // 牛顿
+    // xn+1 = xn - f(xn)/f'(xn)
+    public int mySqrt1(int x) {
+        long r = x;
+        while (r * r > x) {
+            r = (r + x/r) / 2;
+        }
+        return (int)r;
+    }
 }
