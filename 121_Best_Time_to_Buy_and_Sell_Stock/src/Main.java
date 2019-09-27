bb6a12a0b44ccc43041f6228a7b3fda84e04c13a
sleeptodeath <1066049243@qq.com>
2019-09-27 20:58:27 +0800

    add 121

--- 121_Best_Time_to_Buy_and_Sell_Stock/src/Main.java
+++ 121_Best_Time_to_Buy_and_Sell_Stock/src/Main.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] price = {7,6,4,3,1};
+        System.out.println(solution.maxProfit(price));
+    }
 }
--- 121_Best_Time_to_Buy_and_Sell_Stock/src/Solution.java
+++ 121_Best_Time_to_Buy_and_Sell_Stock/src/Solution.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
 
 public class Solution {
+    public int maxProfit(int[] prices) {
+        int ret = 0;
+        if (prices == null || prices.length == 0)
+            return 0;
+        int min = prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] >= min) {
+                ret = Math.max(ret, prices[i] - min);
+            } else {
+                min = prices[i];
+            }
+        }
+        return ret;
+    }
 }
