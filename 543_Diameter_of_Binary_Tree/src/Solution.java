ae2a43784d6c46b8d78c13a7d98655146143da2c
sleeptodeath <1066049243@qq.com>
2019-09-28 10:11:13 +0800

    add 543

--- 543_Diameter_of_Binary_Tree/src/Solution.java
+++ 543_Diameter_of_Binary_Tree/src/Solution.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Solution {
+
+    public int depth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return Math.max(depth(root.left), depth(root.right)) + 1;
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null)
+            return 0;
+        // 包括根节点
+        int m = depth(root.left) + depth(root.right);
+        // 不包括根节点
+        int l = diameterOfBinaryTree(root.left);
+        int r = diameterOfBinaryTree(root.right);
+
+        return Math.max(Math.max(l, r), m);
+    }
 }
--- 543_Diameter_of_Binary_Tree/src/TreeNode.java
+++ 543_Diameter_of_Binary_Tree/src/TreeNode.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) { val = x; }
 }
