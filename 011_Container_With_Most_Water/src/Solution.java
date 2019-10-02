245bbd57fe16a00716cf355ca23644cc173778ac
sleeptodeath <1066049243@qq.com>
2019-10-02 13:12:41 +0800

    add 011

--- 011_Container_With_Most_Water/src/Solution.java
+++ 011_Container_With_Most_Water/src/Solution.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class Solution {
+    // 暴力
+//    public int maxArea(int[] height) {
+//        int ret = 0;
+//        int i, j;
+//
+//        for (i = 0; i < height.length-1; i++) {
+//            for (j = i+1; j < height.length; j++) {
+//
+//                ret = Math.max(ret, (j-i) * Math.min(height[i], height[j]));
+//            }
+//        }
+//        return ret;
+//    }
+
+    public int maxArea(int[] height) {
+       int i = 0, j = height.length-1;
+       int ret = 0;
+       while (i < j) {
+           ret = Math.max(ret, (j-i)*(Math.min(height[i], height[j])));
+           if (height[i] < height[j]) {
+               i++;
+           } else {
+               j--;
+           }
+       }
+       return ret;
+    }
 }
