d047d8772bcfee0a1ed2c795b7247e88c9f1eb57
sleeptodeath <1066049243@qq.com>
2019-10-06 16:12:50 +0800

    add 019

--- 019_Remove_Nth_Node_From_End_of_List/src/Solution.java
+++ 019_Remove_Nth_Node_From_End_of_List/src/Solution.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
 
 public class Solution {
+//    public ListNode removeNthFromEnd(ListNode head, int n) {
+//        if (head == null)
+//            return null;
+//
+//        ListNode H = new ListNode(0);
+//        H.next = head;
+//        ListNode p = head, q= null;
+//        int len = 0;
+//        while (p != null) {
+//            len++;
+//            p = p.next;
+//        }
+//        len -= n;
+//        q = head;
+//        p = H;
+//        while (len-->0) {
+//            q = q.next;
+//            p = p.next;
+//        }
+//        p.next = q.next;
+//        return H.next;
+//    }
+
+    // one pass
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null)
+            return null;
+
+        ListNode H = new ListNode(0);
+        H.next = head;
+        ListNode p = H, q = H;
+
+        while (n-- > 0 ) {
+            p = p.next;
+        }
+        while (p.next != null) {
+            q = q.next;
+            p = p.next;
+        }
+        q.next = q.next.next;
+        return H.next;
+    }
 }
