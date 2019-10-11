aaea8287c78abc4ff0c47df6f525c5cdb9b1cae4
sleeptodeath <1066049243@qq.com>
2019-10-11 18:30:50 +0800

    add 050

--- 050_Pow(x,n)/src/Solution.java
+++ 050_Pow(x,n)/src/Solution.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Solution {
+    public double helper(double x, int n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+
+        if (n % 2 == 0)
+            return helper(x*x, n>>>1);
+        else
+            return x * helper(x*x, n>>>1);
+    }
+
+
+    public double myPow(double x, int n) {
+        if (n < 0)
+            return helper(1/x, -n);
+        else
+            return helper(x, n);
+    }
 }
