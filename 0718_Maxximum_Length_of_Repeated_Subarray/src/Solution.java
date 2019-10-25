9de811f9df6737d43b534643a3cdcb80f18e8f31
sleeptodeath <1066049243@qq.com>
2019-10-25 18:39:28 +0800

    add 0718

--- 0718_Maxximum_Length_of_Repeated_Subarray/src/Solution.java
+++ 0718_Maxximum_Length_of_Repeated_Subarray/src/Solution.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Solution {
+    // DP
+    public int findLength(int[] A, int[] B) {
+        int ret = 0;
+
+        int[][] dp = new int[A.length+1][B.length+1];
+
+        for (int i = A.length-1; i >= 0; i--) {
+            for (int j = B.length-1; j >= 0; j--) {
+                if (A[i] == B[j]) {
+                    dp[i][j] = dp[i+1][j+1] + 1;
+                    ret = Math.max(ret, dp[i][j]);
+                }
+
+            }
+        }
+        return ret;
+    }
 }
