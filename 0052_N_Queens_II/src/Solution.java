d1767c2c4bb946feef7164f60b200bcfcb2d6f80
sleeptodeath <1066049243@qq.com>
2019-11-06 14:32:11 +0800

    add 0052

--- 0052_N_Queens_II/src/Solution.java
+++ 0052_N_Queens_II/src/Solution.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class Solution {
+
+    public int totalNQueens(int n) {
+        boolean[] cols = new boolean[n];
+        boolean[] lu = new boolean[2*n];
+        boolean[] ru = new boolean[2*n];
+        return helper(n, 0, cols, lu, ru);
+    }
+
+    public int helper(int n, int row, boolean[] cols, boolean[] lu, boolean[] ru) {
+        if (row == n)
+            return 1;
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (cols[i] || lu[row-i+n-1] || ru[row+i])
+                continue;
+
+            cols[i] = true;
+            lu[row-i+n-1] = true;
+            ru[row+i] = true;
+            cnt += helper(n, row+1, cols, lu, ru);
+            cols[i] = false;
+            lu[row-i+n-1] = false;
+            ru[row+i] = false;
+
+        }
+        return cnt;
+    }
 }
